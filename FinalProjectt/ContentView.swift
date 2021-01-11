164e817f30b1a9ef4caae330faab22fc2538a234
BMZOOO <76647795+BMZOOO@users.noreply.github.com>
2021-01-11 16:23:37 +0300

    Initial Commit

---
+++ FinalProjectt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinalProjectt
+//
+//  Created by Batool  Jawaheri  on 1/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FinalProjectt/FinalProjecttApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjecttApp.swift
+//  FinalProjectt
+//
+//  Created by Batool  Jawaheri  on 1/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjecttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
